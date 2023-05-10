a2b00c33f2e6572f5f81130c094562acd57e01a5
Felipe Tofolo <ftspenha@gmail.com>
2023-05-10 06:07:36 -0300

    test 3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ function NavBar() {
   return (
     <nav className='nav-bar'>
       <ul className='nav-ul'>
-        <li className='nav-li hamburger-menu'>Hello</li>
+        <li className='nav-li hamburger-menu'></li>
         <li className='nav-li logo'></li>
         <li className='nav-li cart-icon'></li>
       </ul>
